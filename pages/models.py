e3713c5abc85f01364c0d6fefa76bbaf3ae02c7b
DarthHamza <hamzamakia@gmail.com>
2018-06-10 13:12:25 +0300

    completed solution for task

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Page(models.Model):
+	title = models.CharField(max_length=120)
+	content = models.TextField()
+	last_update = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse('page-detail', kwargs={'page_id':self.id})
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Page
 
-# Create your views here.
+
+def page_list(request):
+	pages = Page.objects.all()
+	context = {
+		"pages": pages,
+	}
+	return render(request, 'list.html', context)
+
+
+def page_detail(request, page_id):
+	page = Page.objects.get(id=page_id)
+	context = {
+		"page": page,
+	}
+	return render(request, 'detail.html', context)
